3c3746cffa330664ba75e59b851a9c2ce48e9c7a
rztiwari <rztiwari@gmail.com>
2017-03-07 07:49:47 +0000

    Initial Commit
    Initial Commit

---
+++ src/components/posts_index.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Rajiv on 26/02/2017.
+ */
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+// import {bindActionCreators} from 'redux';
+import {fetchPosts} from '../actions/index'
+import {Link} from 'react-router';
+
+class PostsIndex extends Component {
+
+    componentWillMount(){
+        this.props.fetchPosts();
+    }
+
+    render(){
+        return(
+            <div>
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                        Add a Post
+                    </Link>
+                </div>
+                List of blog posts!
+            </div>
+        )
+    }
+}
+
+// function mapDispatchToProps(dispatch){
+//     return bindActionCreators({fetchPosts}, dispatch);
+// }
+
+
+
+export default connect(null, {fetchPosts: fetchPosts})(PostsIndex);
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Rajiv on 26/02/2017.
+ */
+import React from 'react';
+import { Route, IndexRoute} from 'react-router';
+
+import App from './components/app';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+
+
+export default (
+    <Route path="/" component={App}>
+        <IndexRoute component={PostsIndex}/>
+        <Route path="posts/new" component={PostsNew}/>
+    </Route>
+);
